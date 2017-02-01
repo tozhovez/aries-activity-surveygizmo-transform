13630c99ca2cb5f26929edac26ab6195161709bd
byerc <byerca@mail.uc.edu>
2017-02-01 10:57:03 -0500

    Dependencies and logging

--- test/test.js
+++ test/test.js
@@ -1,7 +1,15 @@
 import { assert } from 'chai';
+import _ from 'highland';
+import fs from 'fs';
 import SurveygizmoTransform from '../lib/index.js';
 
 describe('SurveygizmoTransform', function() {
-  it('', function() {
+
+  const stream = _(fs.createReadStream('./test/fixtures/incoming.json'));
+
+  it('', function(done) {
+      const transform = new SurveygizmoTransform();
+      transform.transformObject(stream);
+      done();
   });
 });
