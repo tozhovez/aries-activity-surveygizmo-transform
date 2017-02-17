8dc649d1dfdef609e27ae063292516e3dc12c3d5
collen <collen@astronomer.io>
2017-02-17 14:01:54 -0500

    Added ability for headerType

---
+++ test/test-config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+require('dotenv').config();
+
+const config = {
+    headerType: "qid"
+    }
+
+export default config;
--- test/test.js
+++ test/test.js
@@ -3,12 +3,12 @@ import _ from 'highland';
 import fs from 'fs';
 import response from '../test/fixtures/test-data.json';
 import SurveygizmoTransform from '../lib/index.js';
-
+import config from '../test/test-config.js';
 describe('SurveygizmoTransform', function() {
 
   it('should contain same amount of incoming surveys', function() {
       const transform = new SurveygizmoTransform();
-      const allData = transform.getResponses(response);
+      const allData = transform.getResponses(response, config);
       assert.equal(allData.length, response.data.length);
   });
 });
